958bd020458ddbbf034c9abd3b7af034bc84e62b
Luke Ghenco <lukeghenco@gmail.com>
2017-06-15 21:31:28 -0400

    Remove react-router-dom import in PictureList

--- module-4-review-front-end/src/components/PictureList.js
+++ module-4-review-front-end/src/components/PictureList.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import Picture from './Picture'
-import { Link, Switch, Route } from 'react-router-dom'
 
-export default function PictureList(props){
+export default function PictureList(props) {
   return(
     <div>
-    {renderPictures(props.pictures)}
+      {renderPictures(props.pictures)}
     </div>
   )
 }
 
-function renderPictures(pictures){
- return pictures.map(pic=>(
-    <Picture key={pic.id} info={pic} index={true}/>
- ))
+function renderPictures(pictures) {
+  return pictures.map(pic=> <Picture key={pic.id} info={pic} index={true}/>)
 }
